971292d6210f39935669a842a7f4610b4596bb19
Peerapon Phokum <103744021+peerapon3014@users.noreply.github.com>
2023-02-09 10:19:46 +0700

    edit about data from student to Employee

---
+++ app/src/main/java/com/example/lab7recyclerviewdialog2022/Employee.kt
@@ -0,0 +1,3 @@
+package com.example.lab7recyclerviewdialog2022
+
+data class Employee (val name:String, val gender:String, val email:String, val salary:Int)
---
+++ app/src/main/java/com/example/lab7recyclerviewdialog2022/EmployeeAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.lab7recyclerviewdialog2022
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lab7recyclerviewdialog2022.databinding.StdItemLayoutBinding
+import layout.Student
+import layout.StudentsAdapter
+
+class EmployeeAdapter (val employeeList: ArrayList<Employee>?, val context: Context):
+    RecyclerView.Adapter<EmployeeAdapter.ViewHolder>(){
+
+    class ViewHolder(view: View, val binding: StdItemLayoutBinding):
+        RecyclerView.ViewHolder(view) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeAdapter.ViewHolder {
+        val binding = StdItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return EmployeeAdapter.ViewHolder(binding.root, binding)
+    }
+
+    override fun onBindViewHolder(holder: EmployeeAdapter.ViewHolder, position: Int) {
+        val binding = holder.binding
+
+        binding.txtName.text = "Name: ${employeeList!![position].name}"
+        binding.txtGender.text = "Gender: ${employeeList!![position].gender}"
+        binding.txtEmail.text = "Email: ${employeeList!![position].email}"
+        binding.txtSalary.text = "Salary: ${employeeList!![position].salary}"
+    }
+
+    override fun getItemCount(): Int {
+        return employeeList!!.size
+    }
+
+}
